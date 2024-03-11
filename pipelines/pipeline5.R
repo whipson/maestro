6ac4d7e71bb9a796e31de47019c2a06b010c197d
Ryan Garnett <ryangarnett@Ryans-MacBook-Pro.local>
2024-03-11 16:34:12 -0300

    Added function to utils, check_datetime function, and five pipeline scenarios

---
+++ pipelines/pipeline5.R
@@ -0,0 +1,37 @@
+########################################
+# Pipline 1 -- Monthly GeoMet Climate Monthly
+# Frequency: Monthly
+# Interval: 1
+########################################
+
+########################################
+# SETUP
+library(httr2)
+library(sf)
+
+pdate <- lubridate::floor_date(lubridate::rollback(Sys.Date(), roll_to_first = FALSE), unit = "months")
+pmonth <- lubridate::month(pdate)
+pyear <- lubridate::year(pdate)
+
+
+# Access climate monthly via geomet api
+req <- httr2::request("https://api.weather.gc.ca/collections/climate-daily/items") |>
+  httr2::req_url_query(
+    lang = "en-CA",
+    limit = 31,
+    offset = 0,
+    CLIMATE_IDENTIFIER = 8202251,
+    LOCAL_MONTH = pmonth,
+    LOCAL_YEAR = pyear
+  )
+
+# Perform the request
+resp <- req |>
+  httr2::req_perform()
+
+# Climate station response to data frame
+df <- resp |>
+  httr2::resp_body_string() |>
+  sf::st_read(quiet = TRUE)
+
+df
