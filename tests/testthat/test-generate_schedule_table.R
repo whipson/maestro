d1cd80bdac99eca2c08f98fa116ded3d88e333e9
Will Hipson <will.hipson@hiaa.ca>
2024-03-18 08:16:26 -0300

    function to generate schedule from directory with tests

--- tests/testthat/test-generate_schedule_table.R
+++ tests/testthat/test-generate_schedule_table.R
@@ -1,1 +1,21 @@
+test_that("generate schedule table works on a directory of all good pipelines", {
+  res <- generate_schedule_table(test_path("test_pipelines_all_good"))
+  expect_s3_class(res, "tbl_df")
+  expect_in(
+    c("script_path", "func_name", "frequency", "interval",
+      "start_time", "tz"),
+    names(res)
+  )
+})
 
+test_that("generate schedule table works on a directory of some good pipelines, warns", {
+  res <- generate_schedule_table(test_path("test_pipelines_some_good"))
+  expect_s3_class(res, "tbl_df")
+  expect_gte(nrow(res), 1)
+  expect_in(
+    c("script_path", "func_name", "frequency", "interval",
+      "start_time", "tz"),
+    names(res)
+  )
+}) |>
+  suppressMessages()
---
+++ tests/testthat/test_pipelines_some_good/test_pipeline_daily_bad.R
@@ -0,0 +1,10 @@
+#' Simple mtcars print function
+#'
+#' @batonFrequency 17
+#' @batonInterval
+#' @batonStartTime
+#'
+#' @export
+get_mtcars <- function() {
+  mtcars
+}
