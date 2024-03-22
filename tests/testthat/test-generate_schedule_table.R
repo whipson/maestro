aec3348cc56b6aa802776b1218478d6889f338e8
Will Hipson <will.hipson@hiaa.ca>
2024-03-22 08:41:13 -0300

    modify test for generate_schedule_table

--- tests/testthat/test-generate_schedule_table.R
+++ tests/testthat/test-generate_schedule_table.R
@@ -1,6 +1,7 @@
 test_that("generate schedule table works on a directory of all good pipelines", {
   res <- generate_schedule_table(test_path("test_pipelines_all_good"))
   expect_s3_class(res, "tbl_df")
+  expect_gte(nrow(res), 1)
   expect_in(
     c("script_path", "func_name", "frequency", "interval",
       "start_time", "tz"),
