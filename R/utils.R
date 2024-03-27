1e7054a3b1da9f260f57c56a3bcb7e865b6c0068
Ryan Garnett <ryangarnett@Ryans-MacBook-Pro.local>
2024-03-27 11:48:14 -0300

    Added identify and select pipelines functions and associated test

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,1 @@
-#' round_time
-#'
-#' @param check_datetime
-#' @param unit_value
-#'
-#' @return
-#'
-#' @examples
-round_time <- function(check_datetime, unit_value) {
-  format(
-    as.POSIXct(lubridate::round_date(check_datetime, unit = unit_value), format = "%H:%M"),
-    "%H:%M:%S"
-  )
-}
+
