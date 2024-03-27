5ed3087e44db0339b1c0445d47c1e666b103df5e
Will Hipson <will.hipson@hiaa.ca>
2024-03-27 12:52:59 -0300

    update tests with snapshots

--- tests/testthat/test-identify_pipelines.R
+++ tests/testthat/test-identify_pipelines.R
@@ -7,7 +7,7 @@ test_that(
                                     pipeline_interval = 1,
                                     pipeline_freq = "day",
                                     pipeline_datetime = lubridate::force_tz(lubridate::as_datetime("2024-01-01 05:05:00"), tzone = "America/Halifax")
-                                    )
+    )
 
     res_true <- identify_pipelines(orch_interval = 15,
                                    orch_unit = "mins",
@@ -15,7 +15,7 @@ test_that(
                                    pipeline_interval = 1,
                                    pipeline_freq = "day",
                                    pipeline_datetime = lubridate::force_tz(lubridate::as_datetime("2024-03-21 05:05:00"), tzone = "America/Halifax")
-                                   )
+    )
 
     expect_type(res_false, "logical")
     expect_type(res_true, "logical")
