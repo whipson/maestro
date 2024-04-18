c1bdd777132699148762c0a2174e6e31a338fe21
Will Hipson <will.hipson@hiaa.ca>
2024-04-18 13:59:03 -0300

    fix warning handling in pipes

--- R/latest_errors.R
+++ R/latest_errors.R
@@ -1,4 +1,4 @@
-#' Retrieve latest maestro parsing errors from
+#' Retrieve latest maestro parsing errors
 #'
 #' @return error and trace output
 #' @export
@@ -6,6 +6,18 @@ latest_parsing_errors <- function() {
   maestro_pkgenv$latest_parsing_errors
 }
 
+#' Retrieve latest maestro runtime errors
+#'
+#' @return error message
+#' @export
 latest_runtime_errors <- function() {
   maestro_pkgenv$latest_runtime_errors
+}
+
+#' Retrieve latest maestro runtime warnings
+#'
+#' @return warning messages
+#' @export
+latest_runtime_warnings <- function() {
+  maestro_pkgenv$latest_runtime_warnings
 }
