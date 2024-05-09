429fb22befd77f81663f52bb2cdec83ac8254354
Will Hipson <will.hipson@hiaa.ca>
2024-05-09 08:55:33 -0300

    for testing

---
+++ inst/sample_pipeline_dir/pipe2.R
@@ -0,0 +1,15 @@
+#' Simple mtcars print function
+#'
+#' This is a function that runs every hour starting at
+#' 2024-03-01 09:00:00
+#'
+#' @maestroFrequency day
+#' @maestroInterval 1
+#' @maestroStartTime 2024-03-01 09:00:00
+#' @maestroTz UTC
+#'
+#'
+#' @export
+get_mtcars2 <- function() {
+  mtcars
+}
