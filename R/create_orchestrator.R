2885577a488fc33b152353e6e0e003151712ee00
Will Hipson <will.hipson@hiaa.ca>
2024-05-29 08:17:54 -0300

    unexport create_orchestrator and change names of last_*

--- R/create_orchestrator.R
+++ R/create_orchestrator.R
@@ -2,21 +2,20 @@
 #'
 #' @param open whether or not to open the script upon creation
 #' @param path file path for the orchestrator script
-#' @param extension file extension for the orchestrator (supports R, Quarto, and RMarkdown)
+#' @param type file type for the orchestrator (supports R, Quarto, and RMarkdown)
 #' @param quiet whether to silence messages in the console (default = `FALSE`)
 #'
 #' @return invisible
-#' @export
 create_orchestrator <- function(
     path = "./orchestrator",
-    extension = c("R", "Quarto", "RMarkdown"),
+    type = c("R", "Quarto", "RMarkdown"),
     open = interactive(),
     quiet = FALSE
 ) {
 
-  extension <- match.arg(extension)
+  type <- match.arg(type, choices = c("R", "Quarto", "RMarkdown"))
 
-  template <- ifelse(extension == "R", "orchestrator_template", "orchestrator_template_qmd")
+  template <- ifelse(type == "R", "orchestrator_template", "orchestrator_template_qmd")
 
   script <- readLines(system.file(template, package = "maestro")) |>
     paste(collapse = "\n") |>
@@ -26,7 +25,7 @@ create_orchestrator <- function(
       .null = NULL
     )
 
-  path <- paste0(path, ".", extension)
+  path <- paste0(path, ".", type)
 
   if (file.exists(path)) {
     overwrite <- readline(glue::glue("File {path} already exists. Overwrite? [Y/n]: \n"))
