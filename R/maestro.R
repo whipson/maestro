cbeaaffb4ecd06529ea4f5b8df78632a32bd170f
Will Hipson <will.hipson@hiaa.ca>
2024-05-30 09:48:18 -0300

    add global vars

--- R/maestro.R
+++ R/maestro.R
@@ -10,5 +10,7 @@
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1")  utils::globalVariables(
   c("frequency", "start_time", "tz", "skip", "log_level", "is_scheduled_now",
-    "next_run")
+    "next_run", "frequency_n", "frequency_unit", "errors", "pipe_name",
+    "script_path", "invoked", "success", "pipeline_started", "pipeline_ended",
+    "messages")
 )
