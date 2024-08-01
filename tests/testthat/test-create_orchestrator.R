af285be30c2d3277fcefdbf636396ff9e7923f7a
Will Hipson <will.hipson@hiaa.ca>
2024-08-01 09:59:45 -0300

    improve test coverage of overwriting files/dirs

--- tests/testthat/test-create_orchestrator.R
+++ tests/testthat/test-create_orchestrator.R
@@ -5,3 +5,21 @@ test_that("create_orchestrator creates a new orchestrator", {
   }) |>
     expect_message()
 })
+
+test_that("create_orchestrator aborts if file already exists", {
+
+  withr::with_tempdir({
+    expect_error({
+      dir <- tempdir()
+      create_orchestrator(dir, open = FALSE)
+      create_orchestrator(dir, open = FALSE)
+    }, regexp = "already exists.")
+
+    # Works if overwrite = TRUE
+    expect_message(
+      create_orchestrator(dir, open = FALSE, overwrite = TRUE),
+      regexp = "Overwriting existing"
+    )
+  })
+}) |>
+  suppressMessages()
