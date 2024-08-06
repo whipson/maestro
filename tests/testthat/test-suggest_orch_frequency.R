78c35c2e0ea55f4d3927037d28c5199d73497f71
Will Hipson <will.hipson@hiaa.ca>
2024-08-06 14:21:09 -0300

    use underscores

--- tests/testthat/test-suggest_orch_frequency.R
+++ tests/testthat/test-suggest_orch_frequency.R
@@ -42,6 +42,12 @@ test_that("suggest_orch_frequency gives valid suggestions", {
   )
 })
 
+test_that("suggest_orch_frequency works when check_datetime is a Date", {
+  expect_no_error(
+    suggest_orch_frequency(example_schedule, check_datetime = as.Date("2024-08-06"))
+  )
+})
+
 test_that("suggest_orch_frequency gives expected errors", {
   expect_error({
     suggest_orch_frequency(1)
